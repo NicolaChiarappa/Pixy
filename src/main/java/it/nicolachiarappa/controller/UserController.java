727a57ce956f3a91d7fa9b843c3c4c1bb1ac1afc
Nick <nicolachiarappa2001@gmail.com>
2025-10-15 11:47:36 +0200

    refactor(user): improve endpoints mapping

--- src/main/java/it/nicolachiarappa/controller/UserController.java
+++ src/main/java/it/nicolachiarappa/controller/UserController.java
@@ -3,16 +3,10 @@ import it.nicolachiarappa.web.dtos.user.BaseUserDTO;
 import it.nicolachiarappa.web.dtos.user.request.SignUpRequest;
 import it.nicolachiarappa.web.dtos.user.request.UpdateUserRequest;
 import it.nicolachiarappa.service.UserService;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -20,22 +14,6 @@ public class UserController {
 
 
     private final UserService userService;
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, Object>> handleValidation(ConstraintViolationException ex) {
-        Map<String, Object> body = new HashMap<>();
-
-        List<String> errors = ex.getConstraintViolations()
-                .stream()
-                .map(ConstraintViolation::getMessage)
-                .toList();
-
-        body.put("status", HttpStatus.BAD_REQUEST.value());
-        body.put("error", "Validation failed");
-        body.put("messages", errors);
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
-    }
 
 
     @Autowired
@@ -49,23 +27,32 @@ public class UserController {
     }
 
     @GetMapping
-    public BaseUserDTO getUserByUsername(@RequestParam(value = "username" ) String username){
-        return userService.getUserByUsername(username);
+    public List<BaseUserDTO> getUserByUsername(@RequestParam(value = "username", required = false ) String username){
+        System.out.println(username);
+        if(username.isEmpty()){
+            return userService.getAll();
+        }
+        return List.of(userService.getUserByUsername(username));
     }
+
+
 
     @PostMapping
     public BaseUserDTO addUser(@RequestBody SignUpRequest dto){
         return userService.saveUser(dto);
     }
 
+    @PostMapping("/many")
+    public List<BaseUserDTO> addMany(@RequestBody List<SignUpRequest> requests){
+        return userService.saveMany(requests);
+    }
+
+
     @PatchMapping("/{id}")
     public BaseUserDTO updateUser(@PathVariable(name = "id") Long id, @RequestBody UpdateUserRequest request){
         return userService.updateUser(id, request);
     }
 
-    @GetMapping
-    public List<BaseUserDTO> findMany(){
-        return userService.getAll();
-    }
+
 
 }
