6ca6b6f1d3262409b3f494ef5af4abccb88b2b0e
Nick <nicolachiarappa2001@gmail.com>
2025-12-01 09:48:48 +0100

    refactor(gear): align gear DTO to conventions; add lens upgrade policy class

--- src/main/java/it/nicolachiarappa/context/catalog/application/dtos/GearDTO.java
+++ src/main/java/it/nicolachiarappa/context/catalog/application/dtos/GearDTO.java
@@ -1,15 +1,19 @@
 package it.nicolachiarappa.context.catalog.application.dtos;
 
+import it.nicolachiarappa.context.catalog.domain.model.Gear;
 import it.nicolachiarappa.context.catalog.domain.model.Manufacturer;
 import it.nicolachiarappa.context.catalog.domain.model.Mount;
 import it.nicolachiarappa.context.catalog.domain.model.Status;
+import it.nicolachiarappa.shared.application.dto.DTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Date;
 import java.util.Set;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class GearDTO {
+public class GearDTO extends DTO<Gear> {
     private String name;
     private Manufacturer manufacturer;
     private Set<Mount> mounts;
---
+++ src/main/java/it/nicolachiarappa/context/catalog/domain/model/LensUpgradePolicy.java
@@ -0,0 +1,8 @@
+package it.nicolachiarappa.context.catalog.domain.model;
+
+public class LensUpgradePolicy extends UpgradePolicy<Lens>{
+    @Override
+    public void applyCustom(Lens oldGear, Lens newGear) {
+
+    }
+}
